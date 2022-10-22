a75011c1c1aaa27b98947e7531bf6f959c01a2e6
8n1 <johannes@skamletz.co.at>
2022-10-22 20:14:26 +0200

    moved list of hardware includes into dedicated header file

---
+++ PhobGCC/board_variants.h
@@ -0,0 +1,9 @@
+//Uncomment the appropriate #include line for your hardware by deleting the two slashes at the beginning of the line.
+
+//#include "teensy/Phob1_0Teensy3_2.h"          // For PhobGCC board 1.0 with Teensy 3.2
+//#include "teensy/Phob1_0Teensy3_2DiodeShort.h"// For PhobGCC board 1.0 with Teensy 3.2 and the diode shorted
+//#include "teensy/Phob1_1Teensy3_2.h"          // For PhobGCC board 1.1 with Teensy 3.2
+//#include "teensy/Phob1_1Teensy3_2DiodeShort.h"// For PhobGCC board 1.1 with Teensy 3.2 and the diode shorted
+//#include "teensy/Phob1_1Teensy4_0.h"          // For PhobGCC board 1.1 with Teensy 4.0
+//#include "teensy/Phob1_1Teensy4_0DiodeShort.h"// For PhobGCC board 1.1 with Teensy 4.0 and the diode shorted
+//#include "teensy/Phob1_2Teensy4_0.h"          // For PhobGCC board 1.2.x with Teensy 4.0
--- PhobGCC/common/phobGCC.h
+++ PhobGCC/common/phobGCC.h
@@ -4,14 +4,8 @@
 //Uncomment to get a glowing LED on Teensy 4.
 //#define ENABLE_LED
 
-//Uncomment the appropriate #include line for your hardware by deleting the two slashes at the beginning of the line.
-//#include "../teensy/Phob1_0Teensy3_2.h"          // For PhobGCC board 1.0 with Teensy 3.2
-//#include "../teensy/Phob1_0Teensy3_2DiodeShort.h"// For PhobGCC board 1.0 with Teensy 3.2 and the diode shorted
-//#include "../teensy/Phob1_1Teensy3_2.h"          // For PhobGCC board 1.1 with Teensy 3.2
-//#include "../teensy/Phob1_1Teensy3_2DiodeShort.h"// For PhobGCC board 1.1 with Teensy 3.2 and the diode shorted
-//#include "../teensy/Phob1_1Teensy4_0.h"          // For PhobGCC board 1.1 with Teensy 4.0
-//#include "../teensy/Phob1_1Teensy4_0DiodeShort.h"// For PhobGCC board 1.1 with Teensy 4.0 and the diode shorted
-//#include "../teensy/Phob1_2Teensy4_0.h"          // For PhobGCC board 1.2.x with Teensy 4.0
+//This is where the user has to select/uncomment the hardware they are using 
+#include "../board_variants.h"
 
 #include "structsAndEnums.h"
 #include "filter.h"
